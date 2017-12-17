2ea4925c8136ecc07fd2124f0bfb1f78c0f2c4e2
Nereuss <ingvarolsen57@gmail.com>
2017-12-17 15:30:53 +0100

    initial upload

---
+++ index.php
@@ -0,0 +1,133 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+ 
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="style.css" rel="stylesheet"> -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/fontawesome-all.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+
+ <div class="navbar navbar-default navbar-fixed-top navbar--color--ghost navbar--size--md" data-startColor="navbar--color--ghost" data-startSize="navbar--size--md" data-intoColor="navbar--color--secondary" data-intoSize="navbar--size--m">
+
+ <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mobileNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="#/" class="navbar-brand">Logo</a>
+      </div>
+    
+      <div class="collapse navbar-collapse" id="mobileNav">
+ 
+        <ul class='nav navbar-nav pull-right'>
+        <li>
+          <a class='nav-link'>
+            WALLET DOWNLOAD <i class="fas fa-credit-card"></i>
+          </a>
+        </li>
+        <li>
+          <a class='nav-link'>
+            WHITEPAPER <i class="far fa-newspaper"></i>
+          </a>
+        </li>
+        <li>
+          <a class='nav-link'>
+            GITHUB <i class="fab fa-github-alt"></i>
+          </a>
+        </li>
+        <li>
+          <a class='nav-link'>
+            DISCORD <i class="fab fa-discord"></i>
+          </a>
+        </li>
+        
+      </ul>
+      </div>
+    </div>
+  </div> 
+
+<div class="video-hero jquery-background-video-wrapper demo-video-wrapper" style="height:100%;">
+  <video class="jquery-background-video" autoplay muted loop poster="https://i.imgur.com/LrkhX2T.png">
+    <source src="misc/MolecularBackgroundSmall.mp4" type="video/mp4">
+    <!-- <source src="MolecularBackgroundSmall.webm" type="video/webm">
+    <source src="MolecularBackgroundSmall.ogv" type="video/ogg"> -->
+  </video>
+	  <div class="video-overlay"></div>
+  	<div class="page-width">
+		    <div class="video-hero--content">
+			      <h2 style="color:white; !important">INTRO TEXT and Logo above</h2>
+			      <p>Extra text</p>
+    		</div>
+  	</div>
+</div>
+
+<div class="main-content">
+  <section>
+    <div class="container">
+      <h2>Section title text</h2>
+      <p> This is it. You've made it so far. There is no going back.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta iste molestias vel commodi, quae quaerat ex numquam. Consectetur corporis explicabo, qui labore, repellat dignissimos illo molestias maxime nisi a consequuntur?</p>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fuga autem dolore, ex nobis, iste quasi, asperiores esse repellendus tempore, obcaecati numquam minima maxime! Laboriosam, ut ipsam magnam corporis aut.</p>
+      <h3>TextTitleText</h3>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi placeat dolor molestias facere veritatis culpa amet fugiat debitis dolorem qui quidem consequuntur mollitia, nesciunt pariatur voluptatum! Dolor accusamus labore, sequi.</p>
+    </div>
+  </section>
+  <section>
+    <div class="container">
+      <h2>Roadmap</h2>
+      <p> Some fancy roadmap stuff</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta iste molestias vel commodi, quae quaerat ex numquam. Consectetur corporis explicabo, qui labore, repellat dignissimos illo molestias maxime nisi a consequuntur?</p>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fuga autem dolore, ex nobis, iste quasi, asperiores esse repellendus tempore, obcaecati numquam minima maxime! Laboriosam, ut ipsam magnam corporis aut.</p>
+      <h3>TextTitleText</h3>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi placeat dolor molestias facere veritatis culpa amet fugiat debitis dolorem qui quidem consequuntur mollitia, nesciunt pariatur voluptatum! Dolor accusamus labore, sequi.</p>
+    </div>
+  </section>
+  <section>
+        <div class="container">
+          <h2>White paper section?</h2>
+          <p> This is it. You've made it so far. There is no going back.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta iste molestias vel commodi, quae quaerat ex numquam. Consectetur corporis explicabo, qui labore, repellat dignissimos illo molestias maxime nisi a consequuntur?</p>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fuga autem dolore, ex nobis, iste quasi, asperiores esse repellendus tempore, obcaecati numquam minima maxime! Laboriosam, ut ipsam magnam corporis aut.</p>
+          <h3>TextTitleText</h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi placeat dolor molestias facere veritatis culpa amet fugiat debitis dolorem qui quidem consequuntur mollitia, nesciunt pariatur voluptatum! Dolor accusamus labore, sequi.</p>
+        </div>
+  </section>
+  <section style="background-color:#dddddd;">
+        <div class="container">
+          <h2>Footer</h2>
+          <p>ZealCoin <i class="far fa-copyright"></i></p>
+        </div>
+  </section>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://rawgit.com/BGStock/jquery-background-video/master/jquery.background-video.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
+<script src="js/script.js"></script>
+
+
+  </body>
+</html>
+
